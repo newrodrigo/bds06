55c1f624140a5356bd14762b21b2d3d5d0880c0a
newrodrigo <rodrigocerqueira@outlook.com>
2023-05-26 00:59:57 -0300

    Movie by genre

--- src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -1,8 +1,17 @@
 package com.devsuperior.movieflix.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.devsuperior.movieflix.entities.Movie;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+	@Query("SELECT DISTINCT obj FROM Movie obj INNER JOIN obj.genre cats WHERE "
+			+ "(:genreId IS NULL OR cats.id = :genreId)")
+	Page<Movie> findByGenre(Long genreId, Pageable pageable);
 }
--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -3,20 +3,32 @@ package com.devsuperior.movieflix.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.dto.MovieGenreDTO;
 import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.repositories.MovieRepository;
 import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class MovieService {
-	
+
 	@Autowired
 	private MovieRepository repository;
-	
+
+	@Transactional(readOnly = true)
+	public Page<MovieGenreDTO> findAllPaged(Long genreId, Pageable pageable) {
+        PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("title").ascending());
+	 	Page<Movie> list = repository.findByGenre(genreId, pageRequest);
+	 	return list.map(x -> new MovieGenreDTO(x));
+	}
+
 	@Transactional(readOnly = true)
 	public MovieDTO findById(Long id) {
 		Optional<Movie> obj = repository.findById(id);
